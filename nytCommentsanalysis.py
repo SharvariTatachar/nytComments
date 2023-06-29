2477692f08a9fc3773a116225be8996243ba717e
Sharvari Tatachar <statach1@swarthmore.edu>
2023-06-29 19:50:36 -0400

    cleaned dataset and comment body

--- nytCommentsanalysis.py
+++ nytCommentsanalysis.py
@@ -1,13 +1,11 @@
 import pandas as pd 
 import numpy as np 
-#from sklearn.tree import DecisionTreeClassifier 
 import re 
 import string 
-import nltk 
-#nltk stuff: 
-nltk.download('stopwords')
-stemmer = nltk.SnowbalStemmer("english")
-stopword = set(stopwords.words('english'))
+from nltk.corpus import stopwords
+import nltk
+stemmer = nltk.SnowballStemmer("english")
+stopword = stopwords.words('english')
 
 
 #READING IN ARTICLE DETAILS ------------
@@ -16,27 +14,34 @@ stopword = set(stopwords.words('english'))
 articles = pd.read_csv("/Users/sharrutatachar/Desktop/ArticlesApril2018.csv", 
 usecols = list(range(0,1)) + list(range(4,8)))
 del articles['multimedia']
-#print(articles.head())
+#articles = articles[0:663] #truncate dataset for smaller set  
+lst_topics = ["Politics", "Culture", "National"]
+art_df = articles.query('newDesk in @lst_topics') 
+
 
 #READING IN COMMENT DETAILS ------------
 
 #using articleID, commentBody, and commentID:
 comments = pd.read_csv("/Users/sharrutatachar/Desktop/CommentsApril2018.csv",
 usecols = list(range(1,2)) + list(range(3,5)))
-#print(comments.head())
+
+#get the comments that are relevant to the chosen articles: 
+articleIDs = art_df['articleID'].values.tolist() 
+comments = comments.query('articleID in @articleIDs') #around 24k comments
 
 
-#CLEAN COMMENT BODY ------------
+#CLEAN COMMENT BODY ---------------
 
 def clean(comment): 
     comment = str(comment).lower()
     comment = re.sub('<br/><br/>', '', comment)
+    comment = re.sub('<br/>', '', comment)
     comment = [word for word in comment.split(' ') if word not in stopword]
     comment = " ".join(comment)
-    coment = [stemmer.stem(word) for word in comment.split(' ')]
+    comment = [stemmer.stem(word) for word in comment.split(' ')]
     comment = " ".join(comment)
     return comment 
 
 #call clean on entire comment body column: 
 comments["commentBody"] = comments["commentBody"].apply(clean)
-
+print(comments["commentBody"].head())
