5f378d038d3403829a7d644ee0f31c8f4a7e9f1d
Sharvari Tatachar <statach1@swarthmore.edu>
2023-06-29 18:24:37 -0400

    cleaned comment body

--- nytCommentsanalysis.py
+++ nytCommentsanalysis.py
@@ -2,7 +2,12 @@ import pandas as pd
 import numpy as np 
 #from sklearn.tree import DecisionTreeClassifier 
 import re 
-#import nltk 
+import string 
+import nltk 
+#nltk stuff: 
+nltk.download('stopwords')
+stemmer = nltk.SnowbalStemmer("english")
+stopword = set(stopwords.words('english'))
 
 
 #READING IN ARTICLE DETAILS ------------
@@ -19,4 +24,19 @@ del articles['multimedia']
 comments = pd.read_csv("/Users/sharrutatachar/Desktop/CommentsApril2018.csv",
 usecols = list(range(1,2)) + list(range(3,5)))
 #print(comments.head())
+
+
+#CLEAN COMMENT BODY ------------
+
+def clean(comment): 
+    comment = str(comment).lower()
+    comment = re.sub('<br/><br/>', '', comment)
+    comment = [word for word in comment.split(' ') if word not in stopword]
+    comment = " ".join(comment)
+    coment = [stemmer.stem(word) for word in comment.split(' ')]
+    comment = " ".join(comment)
+    return comment 
+
+#call clean on entire comment body column: 
+comments["commentBody"] = comments["commentBody"].apply(clean)
 
