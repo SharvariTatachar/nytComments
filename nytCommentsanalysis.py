4faf501b015f2096e8b155d0ea99e45859985c74
Sharvari Tatachar <statach1@swarthmore.edu>
2023-06-29 23:05:28 -0400

    finished trump comments category

--- nytCommentsanalysis.py
+++ nytCommentsanalysis.py
@@ -34,7 +34,7 @@ usecols = list(range(1,2)) + list(range(3,5)))
 
 #get the comments that are relevant to the chosen articles: 
 articleIDs = art_df['articleID'].values.tolist() 
-comments = comments.query('articleID in @articleIDs') #around 24k comments
+comments_general = comments.query('articleID in @articleIDs') #around 24k comments
 
 
 #CLEAN COMMENT BODY ---------------
@@ -50,37 +50,51 @@ def clean(comment):
     return comment 
 
 #call clean on entire comment body column: 
-comments["commentBody"] = comments["commentBody"].apply(clean)
-#print(comments["commentBody"].head())
+#comments_general["commentBody"] = comments_general["commentBody"].apply(clean)
+#print(comments_general["commentBody"].head()
 
 
 #CALCULATE OVERALL SENTIMENT SCORES ------------
 
-comments["Positive"] = [sentiments.polarity_scores(i)["pos"] for i in 
-comments["commentBody"]]
-
-comments["Negative"] = [sentiments.polarity_scores(i)["neg"] for i in 
-comments["commentBody"]]
-
-comments["Neutral"] = [sentiments.polarity_scores(i)["neu"] for i in 
-comments["commentBody"]]
-
-comments_polarity = comments[["commentBody", "Positive", "Negative", "Neutral"]]
-
-#print(comments_polarity.head())
-
-pos = sum(comments_polarity["Positive"])
-neg = sum(comments_polarity["Negative"])
-neu = sum(comments_polarity["Neutral"])
-
 def sentiment_score(x, y, z): 
     if (x>y) and (x>z):
-        print("Positive overall. %d" % (x))
+        print("Positive. %d" % (x))
     elif (y>x) and (y>z): 
-        print("Negative overall. %d" % (y))
+        print("Negative. %d" % (y))
     else: 
-        print("Neutral overall. %d, %d, and %d" % (x, y, z))
+        print("Neutral. %d, %d, and %d" % (x, y, z))
 
-sentiment_score(pos, neg, neu) #this is the analysis for all selected comm. 
+def create_sentiment_columns(df): 
 
-#TO-DO: CALCULATE SENTIMENT SCORES FOR EACH CATEGORY AND COMPARE---------
+    df["Positive"] = [sentiments.polarity_scores(i)["pos"] for i in df["commentBody"]]
+
+    df["Negative"] = [sentiments.polarity_scores(i)["neg"] for i in df["commentBody"]]
+
+    df["Neutral"] = [sentiments.polarity_scores(i)["neu"] for i in df["commentBody"]]
+
+    comments_polarity = df[["commentBody", "Positive", "Negative", "Neutral"]]
+
+    #print(comments_polarity.head())
+
+    pos = sum(comments_polarity["Positive"])
+    neg = sum(comments_polarity["Negative"])
+    neu = sum(comments_polarity["Neutral"])
+
+    return sentiment_score(pos, neg, neu) #this is the analysis for all selected comm. 
+
+#create_sentiment_columns(comments_general)
+
+#GETTING CATEGORICAL DATA------------------- 
+
+trump_related = articles.keywords.apply(lambda x: 'Trump, Donald J' in x)
+df_trump = articles[trump_related]
+
+#277 related articles -- now find the related comments: 
+trump_articleIDs = df_trump['articleID'].values.tolist() 
+comments_trump = comments.query('articleID in @trump_articleIDs')
+
+#clean them -- comment out other cleaning steps when running this: 
+comments_trump["commentBody"] = comments_trump["commentBody"].apply(clean) #fix error here
+#print(comments_trump["commentBody"].head())
+
+create_sentiment_columns(comments_trump)
